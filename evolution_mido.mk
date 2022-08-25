95e704ba9a178c0520a29fd3fe8e2a12e9c796c9
NRanjan-17 <nalinishranjan05@gmail.com>
2022-08-25 18:47:43 +0530

    mido: Disable Blur
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- evolution_mido.mk
+++ evolution_mido.mk
@@ -30,6 +30,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 # Custom stuff
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_USES_MINI_GAPPS := true
+TARGET_ENABLE_BLUR := false
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
